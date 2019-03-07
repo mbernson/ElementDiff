f281e8d2dcd12eafe721d9d843bc79680f267c4a
Mathijs Bernson <mathijsb@q42.nl>
2019-03-07 17:09:18 +0100

    Add .xcodeproj

---
+++ Sources/ElementDiff.h
@@ -0,0 +1,19 @@
+//
+//  ElementDiff.h
+//  ElementDiff
+//
+//  Created by Mathijs Bernson on 07/03/2019.
+//  Copyright © 2019 Q42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElementDiff.
+FOUNDATION_EXPORT double ElementDiffVersionNumber;
+
+//! Project version string for ElementDiff.
+FOUNDATION_EXPORT const unsigned char ElementDiffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElementDiff/PublicHeader.h>
+
+
